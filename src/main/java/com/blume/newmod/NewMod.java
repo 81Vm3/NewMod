25b9fd267520f29c9ccb0a34440d3b06a46eeab5
Blume <p3g4sus@foxmail.com>
2021-07-16 18:57:46 +0800

    some mini config changes

---
+++ src/main/java/com/blume/newmod/NewMod.java
@@ -0,0 +1,11 @@
+package com.blume.newmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NewMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello world!!!!BLUMEBLUME!!!!!!!");
+    }
+}
